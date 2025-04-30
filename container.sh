e6628d3627edf2b59c051bab421dd74055c39282
DiegoMayans <dmayansky@itba.edu.ar>
2025-04-30 18:25:06 -0300

    changed startup scripts

--- container.sh
+++ container.sh
@@ -1,1 +1,1 @@
-docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name TPE_Arqui agodio/itba-so:2.0
+docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name TPE_SO agodio/itba-so-multi-platform:3.0
