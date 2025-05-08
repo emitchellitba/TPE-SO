0381f46ba5dc148cd1ea8eecd89312d2af4757cd
DiegoMayans <dmayansky@itba.edu.ar>
2025-05-08 18:10:24 -0300

    agrego memory manager dummy

---
+++ Kernel/lib/dummy_mm.c
@@ -0,0 +1,31 @@
+#include <memory_management.h>
+
+// In this memory manager, a block is a byte of memory (char).
+
+typedef struct MemoryManagerCDT
+{
+	char *next_address
+} MemoryManagerCDT;
+
+MemoryManagerADT kmm_init(void *const restrict memory_to_manage)
+{
+	MemoryManagerCDT *aux = (MemoryManagerCDT *)memory_to_manage;
+
+	aux->next_address = (char *)memory_to_manage;
+	kmalloc((MemoryManagerADT)aux, sizeof(MemoryManagerCDT)); // reserves the space for the CDT
+
+	return (MemoryManagerADT)aux;
+}
+
+void *kmalloc(MemoryManagerADT const restrict memory_manager, const size_t to_allocate)
+{
+	void *ptr = (void *)(memory_manager->next_address);
+	
+	if (ptr == NULL || to_allocate == 0)
+	{
+		return NULL;
+	}
+
+	memory_manager->next_address += to_allocate;
+	return ptr;
+}
