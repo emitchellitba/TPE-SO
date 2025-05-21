f6f1a352b00ba178328a376ab2e4ec5224716853
DiegoMayans <dmayansky@itba.edu.ar>
2025-05-21 18:23:10 -0300

    agrego creacion de proc

--- Kernel/include/proc/process.h
+++ Kernel/include/proc/process.h
@@ -4,37 +4,72 @@
 #include "../ds/queue.h"
 #include <stdint.h>
 
+/*
+  IDEA FUNCIONAMIENTO PROCESOS:
+
+  - Cada proceso tiene un PCB (Process Control Block) que contiene toda la
+  informacion necesaria para su manejo.
+
+  Cuando un proceso termina:
+    1. Se guarda el valor de retorno en el PCB
+    2. Se cambia el estado del proceso a ZOMBIE (termino pero no fue
+  recolectado)
+    3. Se libera la memoria utilizada por el proceso
+
+  Cuando un proceso padre llama a waitpid(pid):
+    1. Se busca el PCB del proceso hijo en la cola de procesos
+    2. Si el proceso hijo no ha terminado, El proceso padre se bloquea y se
+  agrega a la cola de espera del hijo (obs: los hijos tienen un unico padre, por
+  lo que, no es necesario hacer una cola, simplemente basta con que cuando el
+  hijo termine, el kernel despierte al padre si esta dormido). Cuando un proceso
+  hijo termina, el kernel debe despertar al padre y retornar el valor de retorno
+  del hijo en la syscall.
+    3. Si el proceso hijo ha terminado, el padre encuentra que el estado del
+  hijo es ZOMBIE, el kernel agarra el estado retorno del hijo y se lo pasa al
+  padre (valor de retorno de la syscall), luego se liberan los recursos del hijo
+  y se cambia su estado a DEAD (que en realidad significa que el PCB se remueve
+  de manera completa).
+*/
+
 typedef int pid_t;
 typedef int priority_t;
+
+/** Estados de un proceso */
+#define DEAD 0 // Este estado puede ser innecesario
+#define READY 1
+#define RUNNING 2
+#define ZOMBIE 3
+#define BLOCKED 4
+
+#define STACK_SIZE (8192U) /* 8 KiB */
 
 typedef struct proc {
   pid_t pid;
   char *name;
   struct proc *parent; // Puntero al proceso padre
 
-  uint64_t heap_start; // Direccion de inicio del heap
-  uint64_t heap;       // Puntero justo despues del ultimo byte del heap
-                 // Este valor sera incrementado cuando el proceso pida memoria
-
   uint64_t stack_start;   // Comienzo del stack: direccion mas alta
   uint64_t stack_pointer; // Posicion actual del stack
+
+  char **argv;
+  uint64_t argc;
 
   uint64_t entry; // Direccion de inicio del proceso
 
   struct queue
-      wait_queue; // Cola de procesos hijos que hay que esperar con waitpid
+      wait_queue; // Cola de procesos hijos que hay que esperar con wait
 
-  int64_t exit;    // Valor de retorno del proceso
-  uint64_t status; // Estado del proceso (0 = running, 1 = ready, 2 = zombie, 3
-                   // = dead)
-  uint64_t priority;
+  /** Valor de retorno del proceso. -1 indica que no terminó */
+  int exit;
+
+  int status; // Estado del proceso (0 = running, 1 = ready, 2 = zombie)
+  priority_t priority;
 
   // TODO: Agregar fds (ver como agregarlos)
 
   // PARA DEBUGGING
   uint64_t start_time;
   uint64_t cpu_time;
-  uint64_t blocked_by_pid; // PID del proceso que lo bloqueo
 
   /** por si queremos implementar seniales */
   // struct queue *sig_queue; // Cola de señales
--- Kernel/proc/process.c
+++ Kernel/proc/process.c
@@ -1,21 +1,77 @@
+#include "../include/lib/memory_manager.h"
+#include <ds/queue.h>
+#include <lib/logger.h>
+#include <proc/process.h>
 
-// #include "stdint.h"
-// #include "../include/lib/memory_manager.h"
+static int proc_log_level = LOG_DEBUG;
+LOGGER_DEFINE(proc, proc_log, proc_log_level)
 
-// typedef struct process{
-//     int pid;
-//     int ppid;
-//     char name[32];
-//     int state;
-//     int priority;
-//     int stack_size;
-//     void *stack_pointer;
-// } process_t;
+/**
+ * Manejo de PIDS: Por ahora podemos simplemente usar un int con el ultimo pid
+ * utilizado y cada nuevo proceso le asignamos el pid siguiente. En el futuro,
+ * podemos usar una estructura como un bitmap para asignar los pids.
+ */
 
-// typedef uint8_t pid_t; //NO VA ACA
+struct queue *procs = QUEUE_NEW();
+pid_t last_pid = 0;
 
-// process_t pcb_array[100] = {0};
+pid_t proc_pid_alloc() { return last_pid++; }
 
-// pid_t create_proccess(){
+int proc_new(proc_t **ref, char *name, proc_t *parent, uint64_t entry,
+             char **argv, uint64_t argc) {
+  pid_t new_pid = proc_pid_alloc();
 
-// }
+  proc_t *proc = (proc_t *)kmalloc(kernel_mem, sizeof(proc_t));
+  if (!proc) {
+    proc_log(LOG_ERR, "Error allocating process\n");
+    return -1;
+  }
+
+  /** Alloc stack mem for process */
+  proc->stack_start = (uint64_t)kmalloc(kernel_mem, STACK_SIZE);
+  if (!proc->stack_start) {
+    proc_log(LOG_ERR, "Error allocating stack memory for process\n");
+    kmm_free(proc, kernel_mem);
+    return -1;
+  }
+  proc->stack_pointer = proc->stack_start + STACK_SIZE;
+
+  memset(proc, 0, sizeof(proc_t));
+  proc->pid = new_pid;
+  proc->name = name;
+  proc->status = READY;
+  proc->parent = parent;
+  proc->entry = (uint64_t)entry;
+
+  /** Copy argv in kernel heap */
+
+  char **argv_copy =
+      (char **)kmalloc(kernel_mem, sizeof(char *) * argc /* + 1 */);
+  if (!argv_copy) {
+    proc_log(LOG_ERR, "Error allocating argv memory for process\n");
+    kmm_free(proc->stack_start, kernel_mem);
+    kmm_free(proc, kernel_mem);
+    return -1;
+  }
+
+  /** TODO: COPY ARGS INTO ARGV */
+
+  proc->argv = argv_copy;
+  proc->argc = argc;
+
+  struct queue wait_queue = {0};
+  proc->wait_queue = wait_queue;
+
+  proc->exit = -1;
+
+  return 0;
+}
+
+struct proc *proc_pid_find(pid_t pid);
+
+int proc_init(struct proc *proc);
+
+void proc_kill(struct proc *proc);
+
+/** Cuando un proceso termina se debe llamar a esta funcion que se encarga... */
+int proc_reap(struct proc *proc);
